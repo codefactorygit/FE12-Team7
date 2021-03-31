b4c1080a90eccd620a7ce8af2b2ae5464d9c7190
Martin Obermayer <59625525+mobermayer@users.noreply.github.com>
2021-03-31 16:35:19 +0200

    add: comments, naming convention, style synchronisation

--- ProjA-Course/src/app/myHeader/header.component.ts
+++ ProjA-Course/src/app/myHeader/header.component.ts
@@ -6,18 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  //media query from window for tablet - to - desktop size
   mediaQueryMin = window.matchMedia('(min-width: 1051px)');
 
   constructor() {
+    //add listener to window media query
     this.mediaQueryMin.addListener(this.handleTabletChange);
   }
 
   ngOnInit(): void {
   }
 
+  //method show hide mobile navbar
   toggleMobileNav(): void {
+    //mobile nav element
     let navElementMobile: HTMLElement = document.getElementById("navMobile");
 
+    //on click - if visible hide it, else hidden show it 
     if (navElementMobile.style.display === "block") {
       navElementMobile.style.display = "none";
     } else {
@@ -26,8 +31,12 @@ export class HeaderComponent implements OnInit {
 
   }
 
+  //method called by window listener 
+  //handling case: if menu open, screen size is suddenly widened - closes open mobile menue and changes to desktop  
   handleTabletChange(e) {
+    //mobile navbar
     let navbarMobile: HTMLElement = document.getElementById("navMobile");
+    //if screen size matches close mobile navbar
     if (e.matches) {
       navbarMobile.style.display = "none";
     }
